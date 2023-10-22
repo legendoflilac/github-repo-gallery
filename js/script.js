579de6de3d86cb90ac4d2c9aac9eaab9b3f48ac0
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-22 10:11:09 -0500

    Added function to display repo data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //Target overview div
 const overviewDiv = document.querySelector(".overview");
+//Target ul holding repos
+const repoList = document.querySelector(".repo-list");
 const username = "legendoflilac";
 
 const getProfileData = async function() {
@@ -26,3 +28,22 @@ const displayData = function(json) {
 }
 
 getProfileData();
+
+
+const getUserRepos = async function() {
+    const req = await fetch(`https://api.github.com/users/${username}/repos?sort='updated'?per_page=100`);
+    const json = await req.json();
+    displayRepoData(json);
+}
+
+const displayRepoData = function(repos) {
+    
+    for (let item of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${item.name}</h3>`;
+        repoList.append(li);
+    }
+}
+
+getUserRepos();
